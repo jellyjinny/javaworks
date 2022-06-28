0620e95ca844837af6ca15d8a8f50a5ecf9ae8fe
jellyjinny <22cocosky@gmail.com>
2022-06-28 17:52:21 +0900

    2022.06.28 java source file

---
+++ ch01/src/type/ByteExample.java
@@ -0,0 +1,19 @@
+package type;
+
+public class ByteExample {
+	public static void main(String[] args) {
+		byte var1 = -128;
+		byte var2 = -30;
+		byte var3 = 0;
+		byte var4 = 30;
+		byte var5 = 127;
+		//byte var6 = 128;  //byte타입 변수에 허용범위 초과하여 컴파일 에러가 발생
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(var5);
+	}
+
+}
